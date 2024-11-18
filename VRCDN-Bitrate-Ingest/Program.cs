387fd93b0a8f898146a687ad458cc42f06000e62
Luca <luca@perugia.ovh>
2024-11-18 08:49:09 +0100

    Add CI

--- VRCDN-Bitrate-Ingest/Program.cs
+++ VRCDN-Bitrate-Ingest/Program.cs
@@ -1,4 +1,5 @@
-﻿using LucHeart.VRCDN.BI.Utils;
+﻿using System.Diagnostics.CodeAnalysis;
+using LucHeart.VRCDN.BI.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
--- VRCDN-Bitrate-Ingest/Utils/ServiceProviderExtensions.cs
+++ VRCDN-Bitrate-Ingest/Utils/ServiceProviderExtensions.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace LucHeart.VRCDN.BI.Utils;
 
 public static class ServiceProviderExtensions
 {
-    public static T GetOptions<T>(this IServiceProvider serviceProvider) where T : class
+    public static T GetOptions<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] T>(this IServiceProvider serviceProvider) where T : class
     {
         return serviceProvider.GetRequiredService<IOptions<T>>().Value;
     }
