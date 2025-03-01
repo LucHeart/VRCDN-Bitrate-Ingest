33141b8a1658e63d449c7d39bef114a307ab32b6
LucHeart <luc@luc.cat>
2025-03-01 17:54:25 +0100

    Ádd docker compose example

--- VRCDN-Bitrate-Ingest/Program.cs
+++ VRCDN-Bitrate-Ingest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using LucHeart.VRCDN.BI;
 using LucHeart.VRCDN.BI.Utils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,72 +10,60 @@ using Prometheus.Client.MetricPusher;
 using Prometheus.Client.MetricPusher.HostedService;
 using Serilog;
 
-namespace LucHeart.VRCDN.BI;
+var loggerConfiguration = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+    .WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
 
-public static class Program {
-    public static async Task Main(string[] args)
-    {
-        var loggerConfiguration = new LoggerConfiguration()
-            .MinimumLevel.Information()
-            .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
-            .WriteTo.Console(
-                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
-        
-        // ReSharper disable once RedundantAssignment
-        var isDebug = args
-            .Any(x => x.Equals("--debug", StringComparison.InvariantCultureIgnoreCase));
+// ReSharper disable once RedundantAssignment
+var isDebug = args
+    .Any(x => x.Equals("--debug", StringComparison.InvariantCultureIgnoreCase));
 
 #if DEBUG
-        isDebug = true;
+isDebug = true;
 #endif
-        if (isDebug)
-        {
-            Console.WriteLine("Debug mode enabled");
-            loggerConfiguration.MinimumLevel.Verbose();
-        }
-        
-        Log.Logger = loggerConfiguration.CreateLogger();
-        
-        var builder = Host.CreateDefaultBuilder();
-        builder.ConfigureHostConfiguration(config =>
-        {
-            config.AddJsonFile("config.json", optional: true, reloadOnChange: false);
-            config.AddCommandLine(args);
-            config.AddEnvironmentVariables();
-        });
-        
-        builder.ConfigureServices((context, services)  =>
-        {
-            services.AddSerilog();
-
-            services.AddOptions<Config>().Bind(context.Configuration);
-            
-            services.AddMetricFactory();
-
-            services.AddSingleton<IMetricPusher, MetricPusher>(provider =>
-            {
-                var config = provider.GetOptions<Config>();
-                return new MetricPusher(new MetricPusherOptions
-                {
-                    Endpoint = config.Prometheus.Endpoint,
-                    AdditionalHeaders = config.Prometheus.Headers.Select(x => x),
-                    Job = "vrcdn-bitrate-ingest",
-                    CollectorRegistry = provider.GetRequiredService<ICollectorRegistry>()
-                });
-            });
-            
-            services.AddSingleton<IHostedService, MetricPusherService>(provider => new MetricPusherService(provider.GetRequiredService<IMetricPusher>(), TimeSpan.FromSeconds(2)));
-            services.AddHostedService<RecorderManager>();
-        });
-
-        var app = builder.Build();
-        
-        await app.RunAsync();
-    }
-    
-
-
-
+if (isDebug)
+{
+    Console.WriteLine("Debug mode enabled");
+    loggerConfiguration.MinimumLevel.Verbose();
 }
 
+Log.Logger = loggerConfiguration.CreateLogger();
 
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureHostConfiguration(config =>
+{
+    config.AddJsonFile("config.json", optional: true, reloadOnChange: false);
+    config.AddCommandLine(args);
+    config.AddEnvironmentVariables();
+});
+
+builder.ConfigureServices((context, services) =>
+{
+    services.AddSerilog();
+
+    services.AddOptions<Config>().Bind(context.Configuration);
+
+    services.AddMetricFactory();
+
+    services.AddSingleton<IMetricPusher, MetricPusher>(provider =>
+    {
+        var config = provider.GetOptions<Config>();
+        return new MetricPusher(new MetricPusherOptions
+        {
+            Endpoint = config.Prometheus.Endpoint,
+            AdditionalHeaders = config.Prometheus.Headers.Select(x => x),
+            Job = config.Prometheus.Job,
+            CollectorRegistry = provider.GetRequiredService<ICollectorRegistry>()
+        });
+    });
+
+    services.AddSingleton<IHostedService, MetricPusherHostedService>(provider =>
+        new MetricPusherHostedService(provider.GetRequiredService<IMetricPusher>(), TimeSpan.FromSeconds(2)));
+    services.AddHostedService<RecorderManager>();
+});
+
+var app = builder.Build();
+
+await app.RunAsync();
